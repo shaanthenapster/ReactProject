cba64ae9fddeec3c1ebd1d75b1b4511bf117a8a6
shaanthenapster <shaanthenapster@gmail.com>
2021-02-15 16:08:30 +0530

    Dom value change on click button: State management
    Signed-off-by: shaanthenapster <shaanthenapster@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import './App.css';
 import React , { Component } from 'react';
 import RawLand from './RawLand/RawLand';
+import rawLand from './RawLand/RawLand';
 
 class App extends Component{
+
+  state = {
+    rawLand: [
+      {name : 'Frankford' , location : 'New York'},
+      {name : 'Berlin' , location : 'Switzerland'},
+      {name : 'Brazil' , location : 'North America'},
+      {name : 'Los Angeles' , location : 'U.S.A'},
+    ],
+    plottingLand : 'A different Section of Land Auction'
+  };
+
+  switchNameHandler = () => {
+    console.log(rawLand); 
+    this.setState({
+      rawLand: [
+        {name : 'Bermingham' , location : 'New York'},
+        {name : 'Zurich' , location : 'Switzerland'},
+        {name : 'Brazil' , location : 'North America'},
+        {name : 'Los Angeles' , location : 'South America'},
+      ]
+    });
+  };
+
+
+
   render(){
     return(
       <div className="App">
         <h1>This is my 1st component implementation.</h1>
         <p> When you implement someting , you need to code it properly.</p>
-        <RawLand name = "Dhaulas" location = "Dehradun"/>
-        <RawLand name = "Phulwari" location = "Patna"/>
-        <RawLand name = "Ghaziabad" location = "NCR"/>
-        <RawLand name = "Ghitorni" location = "UttarPradesh"/>
+        <button onClick={this.switchNameHandler}> Click Me </button>
+        <RawLand name ={this.state.rawLand[0].name} location = {this.state.rawLand[0].location}/>
+        <RawLand name = {this.state.rawLand[1].name} location = {this.state.rawLand[0].location}/>
+        <RawLand name = {this.state.rawLand[2].name} location = {this.state.rawLand[0].location}/>
+        <RawLand name = {this.state.rawLand[3].name} location = {this.state.rawLand[0].location}/>
       </div>
     );
-
-    // return React.createElement('div' , {className: 'App'} , 
-    // React.createElement('h1' ,null, 'Hi i\'m from the react create element'));
   }
 }
+
 
 export default App;
