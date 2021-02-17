5a5c1bc72b6c6f4798c03cb6802841ec57ff3cbb
shaanthenapster <shaanthenapster@gmail.com>
2021-02-17 23:42:14 +0530

    passing methods as a parameter to other componenets via props.
    Signed-off-by: shaanthenapster <shaanthenapster@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
-import './App.css';
-import React ,  { useState } from 'react';
-import RawLand from './RawLand/RawLand';
+import "./App.css";
+import React, { useState } from "react";
+import RawLand from "./RawLand/RawLand";
 
-  // In this piece of code we have implemented functional based component , in place of class based component.
-  // We are using ES6 arrow functions  
+// In this piece of code we have implemented functional based component ,
+//in place of class based component.
+// We are using ES6 arrow functions
 
-  const App = props => {
+const App = (props) => {
+  const [rawLandState, setRawLandState] = useState({
+    rawLand: [
+      { name: "Frankford", location: "New York" },
+      { name: "Berlin", location: "Switzerland" },
+      { name: "Brazil", location: "North America" },
+      { name: "Los Angeles", location: "U.S.A" },
+    ],
+    plottingLand: "A different Section of Land Auction",
+  });
 
-  const [rawLandState , setRawLandState] = useState({
-  rawLand: [
-    {name : 'Frankford' , location : 'New York'},
-    {name : 'Berlin' , location : 'Switzerland'},
-    {name : 'Brazil' , location : 'North America'},
-    {name : 'Los Angeles' , location : 'U.S.A'},
-      ],
-     plottingLand : 'A different Section of Land Auction'
-   });
+  console.log(rawLandState);
 
-  console.log(rawLandState)
-   
-   const switchNameHandler = () => {
+  const switchNameHandler = () => {
     setRawLandState({
       rawLand: [
-        {name : 'Bermingham' , location : 'New York'},
-        {name : 'Zurich' , location : 'Switzerland'},
-        {name : 'Brazil' , location : 'North America'},
-        {name : 'Los Angeles' , location : 'South America'},
-      ]
+        { name: "Bermingham", location: "New York" },
+        { name: "Zurich", location: "Switzerland" },
+        { name: "Brazil", location: "North America" },
+        { name: "Los Angeles", location: "South America" },
+      ],
     });
   };
 
-    // useState returns an array with exactly 2 elements i.e:
-    // 1. it will be the current state 
-    //2 . a function that will allow us to update the state.
-   
-    return(
-      <div className="App">
-        <h1>This is my 1st component implementation.</h1>
-        <p> When you implement someting , you need to code it properly.</p>
-        <button onClick={switchNameHandler}> Click Me </button>
-        <RawLand name ={rawLandState.rawLand[0].name} location = {rawLandState.rawLand[0].location}/>
-        <RawLand name = {rawLandState.rawLand[1].name} location = {rawLandState.rawLand[0].location}/>
-        <RawLand name = {rawLandState.rawLand[2].name} location = {rawLandState.rawLand[0].location}/>
-        <RawLand name = {rawLandState.rawLand[3].name} location = {rawLandState.rawLand[0].location}/>
-      </div>
-    );
-  }
+  // useState returns an array with exactly 2 elements i.e:
+  // 1. it will be the current state
+  //2 . a function that will allow us to update the state.
+
+  return (
+    <div className="App">
+      <h1>This is my 1st component implementation.</h1>
+      <p> When you implement someting , you need to code it properly.</p>
+      <button onClick={switchNameHandler}> Click Me </button>
+      <RawLand
+        name={rawLandState.rawLand[0].name}
+        location={rawLandState.rawLand[0].location}
+      />
+      <RawLand
+        name={rawLandState.rawLand[1].name}
+        location={rawLandState.rawLand[0].location}
+      />
+      <RawLand
+        name={rawLandState.rawLand[2].name}
+        location={rawLandState.rawLand[0].location}
+      />
+      <RawLand
+        name={rawLandState.rawLand[3].name}
+        location={rawLandState.rawLand[0].location}
+        click={switchNameHandler} >Circle Rate : 78000
+       </RawLand>
+    </div>
+  );
+};
 
 export default App;
--- src/RawLand/RawLand.js
+++ src/RawLand/RawLand.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const rawLand = (props) => {
-     return <h3> The {props.name} , is located at {props.location} ,
+     return <h3 onClick={props.click}> The {props.name} , is located at {props.location} ,
       with an area of {Math.floor(Math.random() * 5678 )} square ft.</h3>
 }
 
